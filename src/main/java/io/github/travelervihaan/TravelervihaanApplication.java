094608a96871381034dcd914221d6cf503d7a06a
TravelerVihaan <lukasz.biel@10g.pl>
2020-04-27 10:27:29 +0200

    Initial commit

---
+++ src/main/java/io/github/travelervihaan/TravelervihaanApplication.java
@@ -0,0 +1,13 @@
+package io.github.travelervihaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelervihaanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelervihaanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/travelervihaan/TravelervihaanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.travelervihaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelervihaanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
