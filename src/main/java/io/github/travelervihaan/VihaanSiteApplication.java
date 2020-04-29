1aebaa3a32f573f2be3bec309849be314aee280a
TravelerVihaan <lukasz.biel@10g.pl>
2020-04-29 15:02:06 +0200

    MainClass name

--- src/main/java/io/github/travelervihaan/VihaanSiteApplication.java
+++ src/main/java/io/github/travelervihaan/VihaanSiteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelervihaanApplication {
+public class VihaanSiteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TravelervihaanApplication.class, args);
+        SpringApplication.run(VihaanSiteApplication.class, args);
     }
 
 }
